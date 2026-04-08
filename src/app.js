7bb1862d1a32389be72912690bd584ad423d994f
Kaushik Mudaliyar <kaushikmudaliyar1484@gmail.com>
2026-04-08 15:18:04 +0530

    add cors, helmet, morgan, express-rate limit package and add it to app.js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,26 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import { errorHandler } from "./middleware/error.middleware.js";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import rateLimit from "express-rate-limit";
+
 const app = express();
+
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+
+app.use(limiter);
+
 // import routes
 import userRouter from "./routes/user.routes.js";
 import recordRouter from "./routes/record.routes.js";
